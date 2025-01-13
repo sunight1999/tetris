1d1d5aec26b52e47138534326ddfa80452f86fb1
sunight1999 <sunight1999@gmail.com>
2025-01-14 00:04:58 +0900

    블록 회전 기능 구현

--- Assets/1_Scripts/TetrisDefine.cs
+++ Assets/1_Scripts/TetrisDefine.cs
@@ -10,6 +10,7 @@ public struct TetrisBlock
     public Color color;
     public bool[] blockShape;
     public int height;
+    public int width;
     
     public TetrisBlock(int row = TetrisDefine.TetrisBlockRows, int col = TetrisDefine.TetrisBlockCols)
     {
@@ -17,11 +18,45 @@ public struct TetrisBlock
         color = Color.gray;
         blockShape = new bool[row * col];
         height = -1;
+        width = -1;
     }
 
-    public void SetHeight(int inHeight)
+    public void CaculateSize()
     {
-        height = inHeight;
+        for (int i = 0; i < TetrisDefine.TetrisBlockRows; i++)
+        {
+            int offset = TetrisDefine.TetrisBlockCols * i;
+            for (int j = 0; j < TetrisDefine.TetrisBlockCols; j++)
+            {
+                if (!blockShape[offset + j])
+                    continue;
+
+                height = i;
+                width = width < j ? j : width;
+            }
+        }
+
+        // 0부터 시작하는 인덱스가 아니라 실제 길이를 원하므로 1씩 추가
+        ++height;
+        ++width;
+    }
+
+    public void Rotate()
+    {
+        bool[] newBlockShape = new bool[TetrisDefine.TetrisBlockRows * TetrisDefine.TetrisBlockCols];
+
+        for (int i = 0; i < height; i++)
+        {
+            int offset = TetrisDefine.TetrisBlockCols * i;
+            for (int j = 0; j < width; j++)
+            {
+                int targetIndex = (TetrisDefine.TetrisBlockCols * j) + (height - i - 1);
+                newBlockShape[targetIndex] = blockShape[offset + j];
+            }
+        }
+
+        newBlockShape.CopyTo(blockShape, 0);
+        CaculateSize();
     }
 }
 
@@ -30,7 +65,7 @@ public class TetrisDefine : SingletonBehaviour<TetrisDefine>
     public const int TetrisStageRows = 20;
     public const int TetrisStageCols = 10;
     public const int TetrisBlockRows = 4;
-    public const int TetrisBlockCols = 3;
+    public const int TetrisBlockCols = 4;
 
     public static readonly Color NormalCellColor = new Color(0f, 0f, 0f, 0.5f);
     
@@ -43,18 +78,11 @@ public class TetrisDefine : SingletonBehaviour<TetrisDefine>
         for (int i = 0; i < tetrisBlocks.Length; i++)
         {
             TetrisBlock block = tetrisBlocks[i];
-            int lastCellIndex = -1;
-            for (int j = 0; j < block.blockShape.Length; j++)
-            {
-                if (block.blockShape[j])
-                    lastCellIndex = j;
-            }
 
-            if (lastCellIndex == -1)
-                Debug.LogWarning($"{block.name} 블록의 모양이 제대로 설정되지 않았습니다.");
-            else
-                block.SetHeight((int)(lastCellIndex / TetrisBlockCols) + 1);
+            if (block.blockShape.Length != TetrisDefine.TetrisBlockRows * TetrisDefine.TetrisBlockCols)
+                Debug.LogWarning($"{block.name} 블록의 크기가 잘못되었습니다. {TetrisDefine.TetrisBlockRows}x{TetrisDefine.TetrisBlockCols} 크기인지 확인해주세요.");
 
+            block.CaculateSize();
             tetrisBlocks[i] = block;
         }
     }
